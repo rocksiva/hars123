0ee8066a123864cf962ae71a9bad4904f86bb8f4
renuharsh <52622562+renuharsh@users.noreply.github.com>
2019-07-22 16:00:00 +0530

    Create kgk.py

---
+++ kgk.py
@@ -0,0 +1,14 @@
+    
+x=int(input())
+y=list(map(int,input().split()))
+a1=0
+v=[]
+for i in range(0,c):
+	if(d[i]==i):
+		temp=d[i]
+		a1=1
+		v.append(temp)
+		v=sorted(v)
+print(*v)
+if(a1==0):
+	print(-1)
